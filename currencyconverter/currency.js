086ff2d9f586f49f906abf8c0da07562fdfb0fb9
naseemwali <naseemwalikhan776@gmail.com>
2025-07-12 21:24:26 +0500

    addedonagain12july

--- currencyconverter/currency.js
+++ currencyconverter/currency.js
@@ -1,17 +1,33 @@
 
-
-
 const populate= async (value,fromcurr)=>{
-const url='https://api.currencyapi.com/v3/latest?api=cur_live_P3IIGzKUreat2tPHm0wyc6bjLm7FNG4Y9pMUpWgH'
+    let mystr="";
+const url='https://api.currencyapi.com/v3/latest?apikey=cur_live_P3IIGzKUreat2tPHm0wyc6bjLm7FNG4Y9pMUpWgH'
 const response=await fetch(url);
 const result= await response.json();
  console.log(result);
   
 for (const element of Object.keys(result['data'])) {
-    console.log(element);
+//  first method but we need to apply tofixed on values to make it to two digits  so there are arrays 
+// element,result['data'][element]['code'],result['data'][element]['value']
+
+
+    // mystr+= `<tr>
+    //     <td>${element}</td>
+    //     <td>${result['data'][element]['code']}</td>
+    //     <td>${result['data'][element]['value']}</td>
+    //      </tr> `
     
-    element,result['data'][code]
+
+    //  second method 
+    const currencydata=result['data'][element]
+    mystr+= `<tr>
+    <td>${element}</td>
+    <td>${currencydata.code}</td>
+    <td>${Math.round(value * currencydata.value)}</td>
+    </tr> `
 }
+const tableBody=document.querySelector('tbody');
+tableBody.innerHTML=mystr;
 
 }
 
@@ -19,10 +35,10 @@ for (const element of Object.keys(result['data'])) {
 const btny=document.getElementById('btny')
 btny.addEventListener('click',(e)=>{
 e.preventDefault();
-const inputAmount=document.querySelector("input[name='quantity']").value
+const inputAmount= parseInt(document.querySelector("input[name='quantity']").value)
 console.log(inputAmount,'input value is');
 
-const fromcurr=document.querySelector("input[name='fromcurrency']").value
+const fromcurr=document.querySelector("select[name='fromcurrency']").value
 console.log(fromcurr,'first curr is');
 
 
@@ -35,6 +51,5 @@ populate(inputAmount,fromcurr)
 
 
 
-const tableBody=document.querySelector('tbody');
-tableBody.innerHTML=mystr;
+
 
